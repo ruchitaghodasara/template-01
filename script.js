6879cf35aa488f96078f0872f6a42d977df61f6a
Ghodasara <ruchita.v.ghodasara@accenture.com>
2020-12-28 11:46:37 +0530

    add loader img

--- script.js
+++ script.js
@@ -1,1 +1,78 @@
-console.log("Hello Browser")
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+
+// Unsplash API
+const count = 10;
+const apiKey = "jFgS8tteGD425f4oZfygQVaVnD6gt6GucN2yyz3xFek"; 
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`; 
+
+// check if all images were loaded
+function imageLoaded() {
+    imagesLoaded++;
+    console.log('Images Loaded')
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+        console.log('ready = ', ready)
+    }
+}
+
+// Helper Function
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
+}
+
+// Create elements of links & photos , add to dom 
+function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    console.log("Total Images ", totalImages);
+    photosArray.forEach((photo) => {
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+
+        const img = document.createElement('img');
+        setAttributes(img, {
+          src: photo.urls.regular,
+          alt: photo.alt_description,
+          title: photo.alt_description,
+        });
+
+        img.addEventListener('load', imageLoaded);
+
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+// get photos from API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+    } catch (error) {
+        
+    }
+}
+
+// check to see if scrolling near bottom of the page, load more photos
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+    }
+})
+
+// On load
+getPhotos();
